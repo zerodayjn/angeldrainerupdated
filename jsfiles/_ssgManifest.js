53e798df89ac47098c422c6b14c2c84ee5e9fb31
ZERO DAY JN <jsbtc1@proton.me>
2025-05-09 00:46:04 +0200

    Add files via upload

---
+++ jsfiles/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Freferral","\u002Frewards","\u002Fsettings","\u002Fwithdrawals\u002F[mode]","\u002Fwrap\u002F[[...mode]]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
